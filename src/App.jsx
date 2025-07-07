fe54dd5465d5245a13e9e38fb304e3df17433be8
Aniket Mahadik <aniketmahadik212@gmail.com>
2025-07-07 16:08:12 +0530

    handled login Bug

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,21 @@ import appStore from "./utils/appStore";
 import Body from "./components/Body";
 import Login from "./components/Login";
 import Feed from "./components/Feed";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <>
-    <Provider store={appStore}>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Body />}>
-            <Route path="/" element={<Feed />}/>
-            <Route path="/login" element={<Login />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/" element={<Feed />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </Provider>
     </>
   );
