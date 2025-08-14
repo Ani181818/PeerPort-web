844ac48af5addf4e63871c8d7c3ce10af04a4302
Aniket Mahadik <aniketmahadik212@gmail.com>
2025-08-15 00:47:51 +0530

    few Updates

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,13 +5,12 @@ import { BASE_URL } from "../utils/constants";
 import { addUser } from "../utils/userSlice";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 const Body = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const userData = useSelector((store) => store.user);
-  const [loading, setLoading] = useState(true);
 
   const fetchData = async () => {
     try {
@@ -24,26 +23,14 @@ const Body = () => {
         navigate("/");
       }
       console.error(error);
-    } finally {
-      setLoading(false);
     }
   };
 
   useEffect(() => {
     if (!userData || Object.keys(userData).length === 0) {
       fetchData();
-    } else {
-      setLoading(false);
     }
   }, []);
-
-  if (loading) {
-    return (
-      <div className="flex items-center justify-center min-h-screen bg-gray-900 text-white">
-        <span className="loading loading-spinner loading-lg"></span>
-      </div>
-    );
-  }
 
   return (
     <div className="min-h-screen flex flex-col">
